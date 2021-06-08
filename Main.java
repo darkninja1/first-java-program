0f8d93c5f32196d71dba9309bf12afd81b518bcb
Dominik Honzak <>
2021-06-08 12:50:06 +0000

    ...

--- Main.java
+++ Main.java
@@ -10,22 +10,27 @@ import java.io.IOException;
 class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
+    System.out.println("Enter username:");
 
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
-  }
-}
-public class WriteToFile {  
-  public static void main(String[] args) {  
+    String userName = myObj.nextLine(); 
+
+    
+    System.out.println("Enter Password:");
+
+    String passWord = myObj.nextLine(); 
     try {  
-      FileWriter myWriter = new FileWriter("filename.txt");
-      myWriter.write(userName);
+      FileWriter myWriter = new FileWriter("user.txt");
+      myWriter.write(userName+"\n");
       myWriter.close();
       System.out.println("Successfully wrote to the file.");
-    } catch (IOException e) {
+      FileWriter myWriter2 = new FileWriter("pass.txt");
+      myWriter2.write(passWord+"\n");
+      myWriter2.close();
+      System.out.println("Successfully wrote to the file.");
+    } 
+    catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     } 
-  }  
-} 
+  }
+}
