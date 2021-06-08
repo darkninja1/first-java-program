e870f66c35ef26254564204a0e2d58f67fe27a0b
Dominik Honzak <>
2021-06-08 12:44:51 +0000

    ...

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
-import java.util.Scanner;  // Import the Scanner class
+import java.util.Scanner;  
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+
+
+
+
 
 class Main {
   public static void main(String[] args) {
@@ -9,3 +16,16 @@ class Main {
     System.out.println("Username is: " + userName);  // Output user input
   }
 }
+public class WriteToFile {  
+  public static void main(String[] args) {  
+    try {  
+      FileWriter myWriter = new FileWriter("filename.txt");
+      myWriter.write(userName);
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    } 
+  }  
+} 
